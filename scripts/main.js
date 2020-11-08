031294802e204ba3f08270c19592de1a9117b14d
Matthew Sykes <mnsykes@gmail.com>
2020-11-08 17:43:25 -0500

    Week10 Complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,6 @@
 $(document).ready(function () {
-	// Click on magnifying glass icon to hide/show search bar
-	$("#search-icon").click(function () {
-		$("#search-bar").toggle();
-	}); // end search bar
+	$(".donate").hide();
+	$("#donate-btn").click(function () {
+		$(".donate").toggle();
+	});
 });
-
-// side nav
-var hamburger = document.getElementById("hamburger");
-var sideNav = document.getElementById("side-nav");
-var xbtn = document.getElementById("xbtn");
-
-hamburger.addEventListener("click", openNav);
-xbtn.addEventListener("click", closeNav);
-
-function openNav() {
-	sideNav.style.display = "block";
-}
-
-function closeNav() {
-	sideNav.style.display = "none";
-}
-// end side nav
